678790c7cc27969c9492785c897f14803e6c68c1
Munir Hossain Juwel <munirhossainjuwel@gmail.com>
2022-12-17 01:39:54 +0600

    create a post and get post like and comment

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWOR}@cluster0.asxi1ae.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.asxi1ae.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,6 +18,11 @@ const client = new MongoClient(uri, {
 
 async function run() {
   const userCollection = client.db("crud-api").collection("userCollection");
+  const socialCollection = client.db("crud-api").collection("socialCollection");
+  const reactCollection = client.db("crud-api").collection("reactCollection");
+  const commentCollection = client
+    .db("crud-api")
+    .collection("commentCollection");
   // post user
   app.post("/users", async (req, res) => {
     /* 
@@ -35,6 +40,7 @@ async function run() {
   // login user
   app.get("/user", async (req, res) => {
     /* 
+        username must should be uniq
         {
             "userName": "developer-munir",
             "password": "password",
@@ -51,7 +57,8 @@ async function run() {
 
   // update password
   app.patch("/users/updatePassword", async (req, res) => {
-    /* 
+    /*
+    when update password send backend user id must
         {
             "id": "639c87f394922c61dd3bbaba",
             "newPassword": "passwordChanged",
@@ -69,6 +76,79 @@ async function run() {
       updatedDoc
     );
     res.send(passwordUpdatedResult);
+  });
+
+  // for social media
+
+  // create a post
+  app.post("/social", async (req, res) => {
+    /* 
+        {
+            "postedUserName": "developer-munir",
+            "postedUserPhoto": "userPhoto here",
+            "post": "post contents whatever you like",
+            "post-time":"10.00 P.M"
+        }
+      */
+
+    const postData = req.body;
+    const insertAPost = await socialCollection.insertOne(postData);
+    res.send(insertAPost);
+  });
+
+  // get all post
+  app.get("/social", async (req, res) => {
+    const query = {};
+    const allPost = await socialCollection.find(query).toArray();
+    res.send(allPost);
+  });
+
+  // get single person single post
+  app.get("/social/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const singlePersonSinglePost = await socialCollection.findOne(query);
+    res.send(singlePersonSinglePost);
+  });
+  // get single person all post
+  app.get("/social/allPosts/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const singlePersonAllPost = await socialCollection.find(query).toArray();
+    res.send(singlePersonAllPost);
+  });
+
+  // react a post
+  app.post("/social/react", async (req, res) => {
+    /* 
+                { 
+                    "postId":"639cc72b8a2f8a45eb1246c3",
+                     "reactedPersonName": "Munir Hossain Juwel",
+                     "reactedUserPhoto": "userPhoto here",
+                     "userId": "639c87f394922c61dd3bbaba",
+                     "reactType":"like"
+                }
+    */
+    const reacts = req.body;
+    const insertAReact = await reactCollection.insertOne(reacts);
+    res.send(insertAReact);
+  });
+
+  // comment a post
+  app.post("/social/comment", async (req, res) => {
+    /* 
+                { 
+                    "postId":"639cc72b8a2f8a45eb1246c3",
+                     "reactedPersonName": "Munir Hossain Juwel",
+                     "reactedUserPhoto": "userPhoto here",
+                     "userId": "639c87f394922c61dd3bbaba",
+                     "Comment-time":"10.00 P.M",
+                     "comment":"comment-type here"
+                }
+    */
+    const comment = req.body;
+    const insertAComment = await commentCollection.insertOne(comment);
+    res.send(insertAComment);
   });
 }
 run().catch((error) => console.log(error));
